2692f561113e417157700b849fcba2c623bf13c4
CptNoHand <B_Marutschke@gmx.de>
2021-10-02 19:44:55 +0200

    added menu

--- config.lua
+++ config.lua
@@ -6,5 +6,6 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01),
+    ['tuner'] = vector3(124.77, -3014.17, 7.04),
 }
