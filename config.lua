feca9ed43ae9283617be2a9368d1788b2ff4e48a
CptNoHand <B_Marutschke@gmx.de>
2021-10-12 02:01:56 +0200

    changed for mlo

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01),
+    ['mechanic'] = vector3(-339.09, -157.49, 44.59),
     ['tuner'] = vector3(124.77, -3014.17, 7.04),
     ['burgershot'] = vector3(-1192.04, -902.476, 13.998),
 }
