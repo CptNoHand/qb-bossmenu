426f82423747a7b3b02a079386b51034953363d6
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-17 22:01:34 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Jobs = {
     ['police'] = vector3(-1113.23, -832.96, 34.36),
-    ['ambulance'] = vector3(270.5, -1363.0, 24.5),
+    ['ambulance'] = vector3(334.86, -593.96, 43.28),
     ['realestate'] = vector3(-714.75, 261.03, 84.14),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
